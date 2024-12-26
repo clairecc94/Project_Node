0032361d437b09b21564707b1c5155300ea09e14
yasmine <yasmine.mmarbani2001@gmail.com>
2024-12-26 22:53:18 +0100

    fix db connection

--- expenses-tracker/src/database/database.ts
+++ expenses-tracker/src/database/database.ts
@@ -1,8 +1,21 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('expense_tracker_db', 'postgres', 'project', {
+const sequelize = new Sequelize('expense_tracker_db', 'learningDbUser', 'learningDbUser', {
     host: 'localhost',
     dialect: 'postgres',
+    port: 5432,
+    logging: console.log,
 });
+
+// Test the connection
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection to PostgreSQL has been established successfully.');
+    })
+    .catch((error) => {
+        console.error('Unable to connect to the database:', error);
+    });
+
 
 export default sequelize;
